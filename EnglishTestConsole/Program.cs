931dcee4cdaa13c0f6800fb80a16e2681e85a3aa
daichihasegawa0923 <punyachi@yahoo.co.jp>
2020-11-07 23:07:48 +0900

    Console 01

---
+++ EnglishTestConsole/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace EnglishTestConsole
+{
+    /// <summary>
+    /// プログラムの主軸となるプログラム
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// プログラム実行時に実行される最初のメソッド
+        /// </summary>
+        /// <param name="args">コマンドライン引数</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("English Test!!");
+            Console.WriteLine("英語");
+            Console.WriteLine("------------------------");
+            Console.WriteLine("1.English");
+            Console.WriteLine("------------------------");
+            Console.WriteLine("2.Japanese");
+            Console.WriteLine("------------------------");
+            Console.WriteLine("3.Chinese");
+            Console.WriteLine("------------------------");
+            Console.WriteLine("Enter your answer");
+            string answer = Console.ReadLine();
+            if(answer == "1")
+            {
+                Console.WriteLine("Good Job!!");
+            }
+            else
+            {
+                Console.WriteLine("Miss!");
+            }
+            Console.WriteLine("Exit to press \"Enter\" key.");
+            Console.ReadLine();
+        }
+    }
+}
